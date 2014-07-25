4ff74614f4e90f66644163074954e5889cabd248
Marius <marius@twostairs.com>
2014-07-25 23:52:52 +0200

    Fixed status in PaperworkHelpers

--- frontend/app/lib/Paperwork/PaperworkHelpers.php
+++ frontend/app/lib/Paperwork/PaperworkHelpers.php
@@ -6,22 +6,19 @@ class PaperworkHelpers {
 
 	public function apiResponse($status, $data)	{
 		$return = array();
-		$returnCode = 400;
 
 		switch($status) {
-			case 0:
+			case 200:
 				$return['success'] = true;
 				$return['response'] = $data;
-				$returnCode = 200;
 			break;
-			case 1:
+			default:
 				$return['success'] = false;
 				$return['errors'] = $data;
-				$returnCode = 400;
 			break;
 		}
 
-		return \Response::json($return, $returnCode);
+		return \Response::json($return, $status);
 	}
 
 }
--- frontend/app/lib/Paperwork/PaperworkHelpersFacade.php
+++ frontend/app/lib/Paperwork/PaperworkHelpersFacade.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Facade;
 
 class PaperworkHelpers extends Facade {
-	const STATUS_SUCCESS = 0;
-	const STATUS_ERROR = 1;
+	const STATUS_SUCCESS = 200;
+	const STATUS_ERROR = 400;
+	const STATUS_NOTFOUND = 404;
 
     protected static function getFacadeAccessor() { return 'paperworkhelpers'; }
 
