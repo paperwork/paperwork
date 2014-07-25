ed496e55a7f54a20c69609387762d0dbf5e80353
Marius <marius@twostairs.com>
2014-07-25 23:54:52 +0200

    Moooaarrr i18n!

--- frontend/app/lang/en/notebooks.php
+++ frontend/app/lang/en/notebooks.php
@@ -4,7 +4,9 @@ return array(
 
 	'all_notes' => 'All Notes',
 	'title_new_notebook' => 'New notebook',
+	'title_edit_notebook' => 'Edit notebook',
 	'notebook_title' => 'Enter notebook title',
 	'add_shortcut' => 'Add this notebook to my shortcuts',
+	'delete_notebook' => 'I want to delete this notebook (+ its contents) and I\'m damn sure about that!',
 
 );
