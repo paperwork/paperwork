08f4114702b937d2bfb8edd5d36bacfa32a0b4ef
Marius <marius@twostairs.com>
2014-08-03 12:06:51 +0200

    Setting to default mysql port

--- frontend/app/config/database.php
+++ frontend/app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
-			'port'		=> '8889',
+			'port'		=> '3306',
 			'database'  => 'paperwork',
 			'username'  => 'paperwork',
 			'password'  => 'paperwork',
