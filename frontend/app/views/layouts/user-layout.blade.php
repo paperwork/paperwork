37f75e765f6ccd3300a16de3ed1c5ec6b3740380
Marius <marius@twostairs.com>
2014-08-17 19:50:13 +0200

    Added favicon.

--- frontend/app/views/layouts/user-layout.blade.php
+++ frontend/app/views/layouts/user-layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+    <link rel="icon" href="/images/favicon.png">
 
     <title>Paperwork</title>
 
