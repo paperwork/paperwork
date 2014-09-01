313da63c9e5ba6843cc9fe1d30aedda3a536a172
Marius <marius@twostairs.com>
2014-09-01 23:39:19 +0200

    More tests.

--- frontend/app/tests/PaperworkHelpersTest.php
+++ frontend/app/tests/PaperworkHelpersTest.php
@@ -8,4 +8,15 @@ class PaperworkHelpersTest extends TestCase {
 		$this->assertEquals(Response::json(array('success' => true, 'response' => array('test' => 'test')), 200), $json);
 	}
 
+	public function testApiResponseErrors()
+	{
+		$json = PaperworkHelpers::apiResponse(400, array('test' => 'test'));
+		$this->assertEquals(Response::json(array('success' => false, 'errors' => array('test' => 'test')), 400), $json);
+	}
+
+	public function testHasUiLanguage()
+	{
+		$this->assertEquals(true, PaperworkHelpers::hasUiLanguage("en"));
+	}
+
 }
