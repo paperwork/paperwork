1b7e8de7467d6a9124efe7280b20bba9738d56a9
Marius <marius@twostairs.com>
2014-09-17 23:29:44 +0200

    Commenting console.info

--- frontend/app/js/paperwork/paperworkFileUploadController.js
+++ frontend/app/js/paperwork/paperworkFileUploadController.js
@@ -11,37 +11,37 @@ paperworkModule.controller('paperworkFileUploadController', ['$scope', '$rootSco
   });
 
   uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
-      console.info('onWhenAddingFileFailed', item, filter, options);
+      // console.info('onWhenAddingFileFailed', item, filter, options);
   };
   uploader.onAfterAddingFile = function(fileItem) {
-      console.info('onAfterAddingFile', fileItem);
+      // console.info('onAfterAddingFile', fileItem);
   };
   uploader.onAfterAddingAll = function(addedFileItems) {
-      console.info('onAfterAddingAll', addedFileItems);
+      // console.info('onAfterAddingAll', addedFileItems);
   };
   uploader.onBeforeUploadItem = function(item) {
-      console.info('onBeforeUploadItem', item);
+      // console.info('onBeforeUploadItem', item);
   };
   uploader.onProgressItem = function(fileItem, progress) {
-      console.info('onProgressItem', fileItem, progress);
+      // console.info('onProgressItem', fileItem, progress);
   };
   uploader.onProgressAll = function(progress) {
-      console.info('onProgressAll', progress);
+      // console.info('onProgressAll', progress);
   };
   uploader.onSuccessItem = function(fileItem, response, status, headers) {
-      console.info('onSuccessItem', fileItem, response, status, headers);
+      // console.info('onSuccessItem', fileItem, response, status, headers);
   };
   uploader.onErrorItem = function(fileItem, response, status, headers) {
-      console.info('onErrorItem', fileItem, response, status, headers);
+      // console.info('onErrorItem', fileItem, response, status, headers);
   };
   uploader.onCancelItem = function(fileItem, response, status, headers) {
-      console.info('onCancelItem', fileItem, response, status, headers);
+      // console.info('onCancelItem', fileItem, response, status, headers);
   };
   uploader.onCompleteItem = function(fileItem, response, status, headers) {
-      console.info('onCompleteItem', fileItem, response, status, headers);
+      // console.info('onCompleteItem', fileItem, response, status, headers);
   };
   uploader.onCompleteAll = function() {
-      console.info('onCompleteAll');
+      // console.info('onCompleteAll');
       paperworkNotesService.getNoteVersionAttachments($rootScope.getNotebookSelectedId(), ($rootScope.getNoteSelectedId(true)).noteId, $rootScope.getVersionSelectedId(true).versionId, function(response) {
         $rootScope.fileList = response;
         uploader.clearQueue();
