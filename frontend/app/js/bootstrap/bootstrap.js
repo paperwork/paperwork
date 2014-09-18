f68e737f7ab04e1453b8204e242720de561b6421
Marius <marius@twostairs.com>
2014-09-18 22:11:57 +0200

    More bugfixing due to CodeKit issues.

--- frontend/app/js/bootstrap/bootstrap.js
+++ frontend/app/js/bootstrap/bootstrap.js
@@ -1,2 +1,2 @@
+// @codekit-append "transition.js", "collapse.js", "dropdown.js", "modal.js", "tab.js";
 // This is just a placeholder, so CodeKit can use it
-// @codekit-append "bootstrap/transition.js", "bootstrap/collapse.js", "bootstrap/dropdown.js", "bootstrap/modal.js", "bootstrap/tab.js";
