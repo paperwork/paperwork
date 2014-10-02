c213e5cab28f700d8b2f8f7a66904d340f00f6a6
Marius <marius@twostairs.com>
2014-10-02 23:45:48 +0200

    Ongoing re-design

--- frontend/app/views/layouts/user-layout.blade.php
+++ frontend/app/views/layouts/user-layout.blade.php
@@ -21,7 +21,7 @@
   <body ng-app="paperworkNotes">
   	<div ng-controller="paperworkConstructorController"></div>
 
-	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
 		<div class="container-fluid">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
