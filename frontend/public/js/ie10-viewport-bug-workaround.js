51121ced5edfa8fb7c815fa829b1ca55318d9c41
Marius <marius@twostairs.com>
2014-12-06 18:06:34 +0100

    Moved ie10-viewport-bla to ltie11compat

--- frontend/public/js/ie10-viewport-bug-workaround.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";if(navigator.userAgent.match(/IEMobile\/10\.0/)){var e=document.createElement("style");e.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}")),document.querySelector("head").appendChild(e)}}();
