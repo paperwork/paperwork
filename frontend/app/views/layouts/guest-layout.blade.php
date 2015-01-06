684a20767eff73257398204f1ed309169ee0be40
Marius <marius@twostairs.com>
2015-01-06 19:39:30 +0100

    Adding app icons/favicons for issue #45.

--- frontend/app/views/layouts/guest-layout.blade.php
+++ frontend/app/views/layouts/guest-layout.blade.php
@@ -6,7 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="/images/favicon.png">
+
+    <meta name="theme-color" content="#03a9f4">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#03a9f4">
+    <!-- <link rel="apple-touch-startup-image" href=""> -->
+
+    <link rel="icon" href="/images/paperwork-icons/favicon-64x64.png">
+    <link rel="icon" sizes="128x128" href="/images/paperwork-icons/favicon-128x128.png">
+    <link rel="icon" sizes="192x192" href="/images/paperwork-icons/favicon-192x192.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/images/paperwork-icons/favicon-60x60.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/images/paperwork-icons/favicon-76x76.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/images/paperwork-icons/favicon-120x120.png">
+    <link rel="apple-touch-icon" sizes="128x128" href="/images/paperwork-icons/favicon-128x128.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/images/paperwork-icons/favicon-152x152.png">
 
     <title>Paperwork</title>
 
