1cff32fbdeea8aa93be8382b8c8803940ae4aa0d
Marius <marius@twostairs.com>
2015-01-06 20:28:49 +0100

    Moving header meta into partial.

---
+++ frontend/app/views/partials/header-sidewide-meta.blade.php
@@ -0,0 +1,20 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="Paperwork (http://paperwork.rocks)">
+
+<meta name="theme-color" content="#03a9f4">
+<meta name="apple-mobile-web-app-status-bar-style" content="#03a9f4">
+<!-- <link rel="apple-touch-startup-image" href=""> -->
+
+<link rel="icon" href="/images/paperwork-icons/favicon-64x64.png">
+<link rel="icon" sizes="128x128" href="/images/paperwork-icons/favicon-128x128.png">
+<link rel="icon" sizes="192x192" href="/images/paperwork-icons/favicon-192x192.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/images/paperwork-icons/favicon-60x60.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/images/paperwork-icons/favicon-76x76.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/images/paperwork-icons/favicon-120x120.png">
+<link rel="apple-touch-icon" sizes="128x128" href="/images/paperwork-icons/favicon-128x128.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/images/paperwork-icons/favicon-152x152.png">
+
+<title>Paperwork</title>
