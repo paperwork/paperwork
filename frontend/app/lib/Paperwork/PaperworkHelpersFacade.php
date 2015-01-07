0907e4e6799efc6496617e4637bd3a66c116494f
Marius <marius@twostairs.com>
2015-01-07 21:33:04 +0100

    Definition of "umask"

--- frontend/app/lib/Paperwork/PaperworkHelpersFacade.php
+++ frontend/app/lib/Paperwork/PaperworkHelpersFacade.php
@@ -7,6 +7,10 @@ class PaperworkHelpers extends Facade {
 	const STATUS_ERROR = 400;
 	const STATUS_NOTFOUND = 404;
 
+	const UMASK_READONLY = 4;
+	const UMASK_READWRITE = 6;
+	const UMASK_OWNER = 7;
+
 	const MULTIPLE_REST_RESOURCE_DELIMITER = ',';
 
     protected static function getFacadeAccessor() { return 'paperworkhelpers'; }
