4111070202d52f4bdd5619f68307886f0baa1be4
Josh Lemer <joshlemer@gmail.com>
2015-01-24 13:17:15 -0600

    Only display admin link for admins

--- frontend/app/views/partials/navigation-main.blade.php
+++ frontend/app/views/partials/navigation-main.blade.php
@@ -2,7 +2,9 @@
 	<li><a href="[[ URL::route("/") ]]" class="transition-effect"><i class="fa fa-book"></i> <span class="hidden-sm">[[Lang::get('keywords.library')]]</span></a></li>
 	<li><a href="[[ URL::route("user/profile") ]]" class="transition-effect"><i class="fa fa-user"></i> <span class="hidden-sm">[[Lang::get('keywords.profile')]]</span></a></li>
 	<li><a href="[[ URL::route("user/settings") ]]" class="transition-effect"><i class="fa fa-cog"></i> <span class="hidden-sm">[[Lang::get('keywords.settings')]]</span></a></li>
-	<li><a href="[[ URL::route("admin/console") ]]" class="transition-effect"><i class="fa fa-cog"></i> <span class="hidden-sm">[[Lang::get('keywords.admin_area')]]</span></a></li>
+	@if (Auth::user()->isAdmin())
+		<li><a href="[[ URL::route("admin/console") ]]" class="transition-effect"><i class="fa fa-star"></i> <span class="hidden-sm">[[Lang::get('keywords.admin_area')]]</span></a></li>
+	@endif
 	<li><a href="[[ URL::route("user/help") ]]" class="transition-effect"><i class="fa fa-question"></i> <span class="hidden-sm">[[Lang::get('keywords.help')]]</span></a></li>
 	<li><a href="[[ URL::route("user/logout") ]]" class="transition-effect"><i class="fa fa-sign-out"></i> <span class="hidden-sm">[[Lang::get('keywords.sign_out')]]</span></a></li>
 </ul>
