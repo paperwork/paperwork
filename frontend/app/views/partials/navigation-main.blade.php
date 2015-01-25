725e553c45df0919f3cdc0e35e40a44d5a86f844
Josh Lemer <joshlemer@gmail.com>
2015-01-25 11:02:05 -0600

    protect Auth::user()->isAdmin()

--- frontend/app/views/partials/navigation-main.blade.php
+++ frontend/app/views/partials/navigation-main.blade.php
@@ -2,7 +2,7 @@
 	<li><a href="[[ URL::route("/") ]]" class="transition-effect"><i class="fa fa-book"></i> <span class="hidden-sm hidden-md">[[Lang::get('keywords.library')]]</span></a></li>
 	<li><a href="[[ URL::route("user/profile") ]]" class="transition-effect"><i class="fa fa-user"></i> <span class="hidden-sm hidden-md">[[Lang::get('keywords.profile')]]</span></a></li>
 	<li><a href="[[ URL::route("user/settings") ]]" class="transition-effect"><i class="fa fa-cog"></i> <span class="hidden-sm hidden-md">[[Lang::get('keywords.settings')]]</span></a></li>
-	@if (Auth::user()->isAdmin())
+	@if (Auth::user() && Auth::user()->isAdmin())
 		<li><a href="[[ URL::route("admin/console") ]]" class="transition-effect"><i class="fa fa-star"></i> <span class="hidden-sm hidden-md">[[Lang::get('keywords.admin_area')]]</span></a></li>
 	@endif
 	<li><a href="[[ URL::route("user/help") ]]" class="transition-effect"><i class="fa fa-question"></i> <span class="hidden-sm hidden-md">[[Lang::get('keywords.help')]]</span></a></li>
