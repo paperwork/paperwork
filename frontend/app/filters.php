07a2ee0e98ed90d9758c83e7d2370293fe79647d
Akbar Hidayat <keripix@gmail.com>
2015-01-26 20:26:02 +0700

    fix csrf filter vulnerability

--- frontend/app/filters.php
+++ frontend/app/filters.php
@@ -116,7 +116,7 @@ Route::filter('guest', function()
 
 Route::filter('csrf', function()
 {
-	if (Session::token() != Input::get('_token'))
+	if (Session::token() !== Input::get('_token'))
 	{
 		throw new Illuminate\Session\TokenMismatchException;
 	}
