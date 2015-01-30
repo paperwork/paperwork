a28eb9bac1cfbb697039454546c1cf4c04a1558d
Haroenv <fingebimus@me.com>
2015-01-30 10:03:52 +0100

    added Dutch

---
+++ frontend/app/lang/nld/messages.php
@@ -0,0 +1,33 @@
+<?php
+
+return array(
+
+	'account_creation_failed' => 'Could not create account.',
+	'account_update_failed' => 'Could not update account.',
+	'invalid_credentials' => 'Your credentials are invalid.',
+	'note_version_info' => 'You are previewing an older version of this note.',
+	'found_bug' => 'Found a bug? <a href="https://github.com/twostairs/paperwork/issues/new" target="_blank" title="Submit Issue">Submit it on GitHub!</a>',
+	'error_message' => 'Whooops!',
+	'onbeforeunload_info' => 'Data will be lost if you leave the page, are you sure?',
+	'user' => array(
+		'settings' => array(
+			'language' => array(
+				'ui_language' => 'User Interface Language',
+				'document_languages' => 'Document Languages',
+				'document_languages_description' => 'The languages you select here will be used for parsing text within attachments you upload, allowing you to search for the content of these. An attachment could be a photo of a document you took with your smartphone, for example. Select the languages these documents are usually written in.',
+			),
+			'client' => array(
+				'qrcode' => 'QR Code',
+				'scan' => 'Scan this QR code with your mobile app to auto-configure your Paperwork account.'
+			),
+			'import' => array(
+				'evernotexml' => 'Evernote XML:',
+				'upload_evernotexml' => 'Upload your Evernote XML export here, to import your Evernote notes into Paperwork.'
+			),
+			'export' => array(
+			    'evernotexml' => 'Export as Evernote XML:',
+			    'download_evernotexml' => 'Download an ENEX file compatible with Evernote to move your notes from Paperwork. '
+			)
+		)
+	)
+);
---
+++ frontend/app/lang/nld/users.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+
+	'title_login' => 'Sign in',
+	'title_register' => 'Register',
+	'title_request' => 'Forgot password',
+	'title_reset' => 'Reset password',
+	'link_forgot_password' => 'Forgot your password?',
+	'profile_change_password_info' => 'To change your account password, please enter a new password here:',
+);
