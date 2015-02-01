b4b8d63baf434f879b20dd96d75f3cba03cff6a2
Chris Lahaye <mail@chrislahaye.com>
2015-02-01 21:08:12 +0100

    Fix searching by tagid
    Match in regular expression that isn't used (and prob. never will) now
    ignored, result indexes are changed. Requests are now made to
    api/v1/tagged/1..9* instead of api/v1/tagged/undefined.

--- frontend/app/js/paperwork/search/search.controller.js
+++ frontend/app/js/paperwork/search/search.controller.js
@@ -5,11 +5,11 @@ paperworkModule.controller('SearchController',
 
      $rootScope.search = sQ;
 
-     var searchMatch = /([a-zA-Z]+)(\:(\d+))?(\/.+)?/g.exec(sQ);
+     var searchMatch = /([a-zA-Z]+)(?:\:(\d+))?(\/.+)?/g.exec(sQ);
      if(typeof searchMatch != "undefined" && searchMatch != null && searchMatch.length > 0) {
        switch(searchMatch[1]) {
          case "tagid":
-           notesService.getNotesInTag(searchMatch[3]);
+           notesService.getNotesInTag(searchMatch[2]);
            break;
          default:
            notesService.getNotesFromSearch(searchMatch[0]);
