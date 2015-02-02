6fee836ccf5be820e80eb1faf52a43db77331a31
Haroenv <fingebimus@me.com>
2015-02-02 11:01:33 +0100

    halfway

--- frontend/app/lang/nl/notebooks.php
+++ frontend/app/lang/nl/notebooks.php
@@ -11,14 +11,17 @@ return array(
     'welcome_notebook_title' => 'Welkom',
     'welcome_note_tag' => 'welkom',
     'welcome_note_content' => 
-        '<p>Hi there, <strong>welcome to Paperwork</strong>, the open source note-taking solution. This '.
-        'is your first note and we&#39;d like to explain to you some features we hope you&#39;ll find very'.
-        'interesting.</p><h3><strong>Notes</strong></h3><p>Notes are the basic form of writing in Paperwork.'.
-        'Notes are saved inside notebooks. You already have one named &#39;Welcome&#39; in which this note is '.
-        'stored. You can create as much notebooks and notes as you want by using the <a id="file_click_handler" href="#">'.
-        'File menu</a>.</p><h3><strong>Attachments</strong></h3><p>You can also add attachments to your notes. '.
-        'This allows you to embed important information and have it there when you need it. Just click its link, '.
-        'and you&#39;ll be sent to the file. And since Paperwork is self hosted, you have better privacy too.</p> '.
+        '<p>Hallo, <strong>welkom bij Paperwork</strong>, de open-source oplossing voor notities. '.
+        'Dit is je eerste notitie en we willen je een paar features die je zeker leuk zal vinden uitleggen. '.
+        '</p><h3><strong>Notities</strong></h3><p>Notities zijn de basisvorm van schrijven bij Paperwork. '.
+        'Notities worden opgeslagen in notitieboeken. Je hebt er al eentje die &#39;Welkom&#39; heet, waarin '.
+        'deze notitie opgeslagen is. Je kan zo veel notitieboeken en notities maken als je wil door het <a id="file_click_handler" href="#">'.
+        'bestand-menu</a> te gebruiken.</p><h3><strong>Bijlagen</strong></h3><p>Je kan ook makkelijk bijlagen '.
+        'aan je notities toevoegen. Dit laat je toe belangrijke bestanden en informatie rechtstreeks in je notities '.
+        'bij te hebben. Als je de link naar het bestand aanklikt, kom je direct bij dat bestand uit. Doordat Paperwork '.
+        'op je eigen machines gehost is, heb je er meer privacy bij dan bij alternatieven.</p><h3><strong>Tags</strong><h3><p>'.
+        ''.
+
         '<h3><strong>Tags</strong></h3><p>Tags are one of Paperwork&#39;s powerful features. Tags allow you to search '.
         'for related notes but which are not in the same notebook. Do you have an idea and want to save it for later now? '.
         'Put it in a notebook (like &#39;dinner cooking&#39;) and tag it with cooking. Later, when you&#39;ve got time you '.
