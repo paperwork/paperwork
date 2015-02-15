0466aa354ea7ce42c74ec3574c838a52a7ad7856
enedil <enedil@outlook.com>
2015-02-15 13:06:39 +0100

    Update users.php

--- frontend/app/lang/pl/users.php
+++ frontend/app/lang/pl/users.php
@@ -6,6 +6,7 @@ return array(
 	'title_register' => 'Zarejestruj się',
 	'title_request' => 'Zapomniałem hasła',
 	'title_reset' => 'Resetuj hasło',
-	'link_forgot_password' => 'Zaponiałeś hasła??',
+	'link_forgot_password' => 'Zapomniałeś hasła?',
+	'label_remember_me' => 'Zapamiętaj',
 	'profile_change_password_info' => 'Aby zmienić swoje hasło, podaj nowe haslo tutaj:',
 );
