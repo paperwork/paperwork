646fd73c7e796866813d4024059b3fb2573959ba
Chris Lahaye <mail@chrislahaye.com>
2015-02-18 23:34:32 +0100

    Add ui.bootstrap to paperworkNotes module

--- frontend/app/js/paperwork/app.js
+++ frontend/app/js/paperwork/app.js
@@ -1,2 +1,2 @@
-angular.module('paperworkNotes', ['ngRoute', 'ngSanitize', 'ngAnimate', 'angularFileUpload', 'ab-base64'])
+angular.module('paperworkNotes', ['ngRoute', 'ngSanitize', 'ngAnimate', 'angularFileUpload', 'ab-base64', 'ui.bootstrap'])
 .constant('paperworkApi', '/api/v1');
