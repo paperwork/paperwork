7381ac68b1d9378693f245f53de641a26c95fb81
Arsenij Łozicki <manwe64@gmail.com>
2015-02-22 01:24:36 +0200

    Renamed directive to have paperwork(pw) namespace prefix

--- frontend/app/views/modal/manageTags.blade.php
+++ frontend/app/views/modal/manageTags.blade.php
@@ -9,7 +9,7 @@
             </div>
             <div class="modal-body">
                 <div class="manage-list-content">
-                    <div class="row" ng-repeat="tag in modalTags | orderBy:'title':reverse" on-finish-render="ngRepeatFinished">
+                    <div class="row" ng-repeat="tag in modalTags | orderBy:'title':reverse" pw-on-finish-render="ngRepeatFinished">
                         <div class="col-sm-10">
                             <a class="line" href="#" data-name="title" data-type="text" data-pk="{{tag.id}}">{{tag.title}}</a>
                         </div>
