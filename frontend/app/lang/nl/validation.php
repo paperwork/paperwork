552e18e7f220192f347aebfa4c47a98359cb7e13
Haroenv <fingebimus@me.com>
2015-03-06 20:58:10 +0100

    De in comments should be The

--- frontend/app/lang/nl/validation.php
+++ frontend/app/lang/nl/validation.php
@@ -94,7 +94,7 @@ return array(
 	| Custom Validation Attributes
 	|--------------------------------------------------------------------------
 	|
-	| De following language lines are used to swap attribute place-holders
+	| The following language lines are used to swap attribute place-holders
 	| with something more reader friendly such as E-Mail Address instead
 	| of "email". This simply helps us make messages a little cleaner.
 	|
