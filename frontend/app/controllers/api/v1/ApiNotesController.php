70b48940fb0aa863aabcc2e1921154b8ec4eebc4
Arsenij Łozicki <manwe64@gmail.com>
2015-03-30 21:10:19 +0300

    #304 Note list error
    Formatting

--- frontend/app/controllers/api/v1/ApiNotesController.php
+++ frontend/app/controllers/api/v1/ApiNotesController.php
@@ -13,9 +13,9 @@ class ApiNotesController extends BaseController
         $tags = array();
         foreach ($note->tags as $tag) {
             $tags[] = array(
-              'id'         => $tag->id,
-              'title'      => $tag->title,
-              'visibility' => $tag->visibility
+                'id'         => $tag->id,
+                'title'      => $tag->title,
+                'visibility' => $tag->visibility
             );
         }
         return $tags;
@@ -24,7 +24,7 @@ class ApiNotesController extends BaseController
     private function getNoteVersionsBrief($noteId)
     {
         $note           =
-          Note::with('version')->where('notes.id', '=', $noteId)->first();
+            Note::with('version')->where('notes.id', '=', $noteId)->first();
         $versionsObject = $note->version()->first();
         if (is_null($versionsObject)) {
             return null;
@@ -39,11 +39,11 @@ class ApiNotesController extends BaseController
             $versionsArray[] = $tmp;
 
             $versions[] = array(
-              'id'          => $tmp->id,
-              'previous_id' => $tmp->previous_id,
-              'next_id'     => $tmp->next_id,
-              'latest'      => $isLatest,
-              'timestamp'   => $tmp->created_at->getTimestamp()
+                'id'          => $tmp->id,
+                'previous_id' => $tmp->previous_id,
+                'next_id'     => $tmp->next_id,
+                'latest'      => $isLatest,
+                'timestamp'   => $tmp->created_at->getTimestamp()
             );
             $isLatest   = false;
             $tmp        = $tmp->previous()->first();
@@ -116,7 +116,7 @@ class ApiNotesController extends BaseController
                             ->get(array('notebookid' => $notebookId))
                             ->toArray();
         return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_SUCCESS,
-          $notes);
+            $notes);
     }
 
     public function search($queryBase64Encoded)
@@ -137,89 +137,107 @@ class ApiNotesController extends BaseController
                        $join->on('notes.version_id', '=', 'versions.id');
                    });
 
-		$dateFilter = preg_match_all("/\s*date:(?:(\d{4})(?:[-\/.](\d{2}))?(?:[\/.-](\d{2}))?)/", $searchQuery, $matches, PREG_SET_ORDER);
+        $dateFilter =
+            preg_match_all("/\s*date:(?:(\d{4})(?:[-\/.](\d{2}))?(?:[\/.-](\d{2}))?)/",
+                $searchQuery, $matches, PREG_SET_ORDER);
 
-		if($dateFilter !== false && $dateFilter > 0) {
-			// Only use last date
-			$date = end($matches);
+        if ($dateFilter !== false && $dateFilter > 0) {
+            // Only use last date
+            $date = end($matches);
 
-			if ($date !== false) {
+            if ($date !== false) {
 
-				$notes = $notes->where(DB::raw('YEAR(notes.updated_at)'), '=', $date[1]);
+                $notes = $notes->where(DB::raw('YEAR(notes.updated_at)'), '=',
+                    $date[1]);
 
-				if(isset($date[2])) {
-					$notes = $notes->where(DB::raw('MONTH(notes.updated_at)'), '=', sprintf("%02s", $date[2]));
-				}
+                if (isset($date[2])) {
+                    $notes =
+                        $notes->where(DB::raw('MONTH(notes.updated_at)'), '=',
+                            sprintf("%02s", $date[2]));
+                }
 
-				if(isset($date[3])) {
-					$notes = $notes->where(DB::raw('DAY(notes.updated_at)'), '=', sprintf("%02s", $date[3]));
-				}
-			}
-
-            $searchQuery =
-              PaperworkHelpers::cleanupMatches($searchQuery, $matches);
-        }
-
-		$filters = preg_match_all("/\s*(tagid|note(?:book)?id)\:([a-f\d]{8}(-[a-f\d]{4}){3}-[a-f\d]{12}?)/", $searchQuery, $matches, PREG_SET_ORDER);
-		if($filters !== false && $filters > 0) {
-			foreach($matches as $match) {
-				switch($match[1]) {
-					case "tagid" :
-						$notes = $notes->join('tag_note', function($join) {
-							$join->on('notes.id', '=', 'tag_note.note_id');
-						})
-						->where('tag_note.tag_id', '=', $match[2]);
-
-						break;
-					case "noteid" :
-						$notes = $notes->where('notes.id', '=', $match[2]);
-
-						break;
-					case "notebookid" :
-						if($match[2] != ApiNotebooksController::NOTEBOOK_ALL_ID) {
-							$notes = $notes->where('notes.notebook_id', '=', $match[2]);
-						}
-
-					break;
-				}
-			}
+                if (isset($date[3])) {
+                    $notes =
+                        $notes->where(DB::raw('DAY(notes.updated_at)'), '=',
+                            sprintf("%02s", $date[3]));
+                }
+            }
 
             $searchQuery =
-              PaperworkHelpers::cleanupMatches($searchQuery, $matches);
+                PaperworkHelpers::cleanupMatches($searchQuery, $matches);
         }
 
-		$notes = $notes->whereNull('notes.deleted_at')
-			->whereNull('notebooks.deleted_at')
-			->where(function($query) use ( &$searchQuery ) {
-				$query->orWhere('versions.title', 'LIKE', '%' . $searchQuery . '%')
-						->orWhere('versions.content', 'LIKE', '%' . $searchQuery . '%')
-						->orWhere('versions.content_preview', 'LIKE', '%' . $searchQuery . '%');
-						// ->orWhere('attachment.content', 'LIKE', '%' . $searchQuery . '%')
-			})
-			->select(
-				'notes.id',
-				'notes.notebook_id',
-			  	'notebooks.title as notebook_title',
-			  	'versions.title',
-			  	'versions.content_preview',
-				'versions.content',
-				'notes.created_at',
-				'notes.updated_at',
-				'note_user.umask'
-			)->distinct()
-			 //->toSql();
-			->get();
+        $filters =
+            preg_match_all("/\s*(tagid|note(?:book)?id)\:([a-f\d]{8}(-[a-f\d]{4}){3}-[a-f\d]{12}?)/",
+                $searchQuery, $matches, PREG_SET_ORDER);
+        if ($filters !== false && $filters > 0) {
+            foreach ($matches as $match) {
+                switch ($match[1]) {
+                    case "tagid" :
+                        $notes = $notes->join('tag_note', function ($join) {
+                            $join->on('notes.id', '=', 'tag_note.note_id');
+                        })
+                                       ->where('tag_note.tag_id', '=',
+                                           $match[2]);
 
-		if(is_null($notes)){
-			return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_NOTFOUND, array());
-		} else {
-			foreach($notes as $note) {
-				$note->tags = $this->getNoteTags($note->id);
-				$note->versions = $this->getNoteVersionsBrief($note->id);
-			}
-			return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_SUCCESS, $notes);
-		}
-	}
+                        break;
+                    case "noteid" :
+                        $notes = $notes->where('notes.id', '=', $match[2]);
+
+                        break;
+                    case "notebookid" :
+                        if ($match[2] !=
+                            ApiNotebooksController::NOTEBOOK_ALL_ID
+                        ) {
+                            $notes = $notes->where('notes.notebook_id', '=',
+                                $match[2]);
+                        }
+
+                        break;
+                }
+            }
+
+            $searchQuery =
+                PaperworkHelpers::cleanupMatches($searchQuery, $matches);
+        }
+
+        $notes = $notes->whereNull('notes.deleted_at')
+                       ->whereNull('notebooks.deleted_at')
+                       ->where(function ($query) use (&$searchQuery) {
+                           $query->orWhere('versions.title', 'LIKE',
+                               '%' . $searchQuery . '%')
+                                 ->orWhere('versions.content', 'LIKE',
+                                     '%' . $searchQuery . '%')
+                                 ->orWhere('versions.content_preview', 'LIKE',
+                                     '%' . $searchQuery . '%');
+                           // ->orWhere('attachment.content', 'LIKE', '%' . $searchQuery . '%')
+                       })
+                       ->select(
+                           'notes.id',
+                           'notes.notebook_id',
+                           'notebooks.title as notebook_title',
+                           'versions.title',
+                           'versions.content_preview',
+                           'versions.content',
+                           'notes.created_at',
+                           'notes.updated_at',
+                           'note_user.umask'
+                       )->distinct()
+            //->toSql();
+                       ->get();
+
+        if (is_null($notes)) {
+            return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_NOTFOUND,
+                array());
+        } else {
+            foreach ($notes as $note) {
+                $note->tags     = $this->getNoteTags($note->id);
+                $note->versions = $this->getNoteVersionsBrief($note->id);
+            }
+            return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_SUCCESS,
+                $notes);
+        }
+    }
 
     public function show($notebookId, $id = null)
     {
@@ -277,17 +295,17 @@ class ApiNotesController extends BaseController
 
         $notes = PaperworkDb::note()
                             ->get(array(
-                              'id' => explode(PaperworkHelpers::MULTIPLE_REST_RESOURCE_DELIMITER,
-                                $id)
+                                'id' => explode(PaperworkHelpers::MULTIPLE_REST_RESOURCE_DELIMITER,
+                                    $id)
                             ));
         // print_r($notes[0]->version()->title);
         if (empty($notes)) {
             return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_NOTFOUND,
-              array());
+                array());
         }
 
         return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_SUCCESS,
-          $notes->first());
+            $notes->first());
 
     }
 
@@ -300,24 +318,25 @@ class ApiNotesController extends BaseController
         $notebook = DB::table('notebooks')
                       ->join('notebook_user', function ($join) {
                           $join->on('notebooks.id', '=',
-                            'notebook_user.notebook_id')
+                              'notebook_user.notebook_id')
                                ->where('notebook_user.user_id', '=',
-                                 Auth::user()->id);
+                                   Auth::user()->id);
                       })
                       ->select('notebooks.id', 'notebooks.type',
-                        'notebooks.title')
+                          'notebooks.title')
                       ->where('notebooks.id', '=', $notebookId)
                       ->whereNull('notebooks.deleted_at')
                       ->first();
 
         if (is_null($notebook)) {
-            return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_NOTFOUND, []);
+            return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_NOTFOUND,
+                []);
         }
 
         $version = new Version([
-          'title'           => $newNote->get("title"),
-          'content'         => $newNote->get("content"),
-          'content_preview' => $newNote->get("content_preview")
+            'title'           => $newNote->get("title"),
+            'content'         => $newNote->get("content"),
+            'content_preview' => $newNote->get("content_preview")
         ]);
 
         $version->save();
@@ -330,7 +349,7 @@ class ApiNotesController extends BaseController
         // TODO: Should we inherit the umask from the notebook?
         $note->users()
              ->attach(Auth::user()->id,
-               array('umask' => PaperworkHelpers::UMASK_OWNER));
+                 array('umask' => PaperworkHelpers::UMASK_OWNER));
 
         $tagIds = ApiTagsController::createOrGetTags($newNote->get('tags'));
 
@@ -339,7 +358,7 @@ class ApiNotesController extends BaseController
         }
 
         return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_SUCCESS,
-          $note);
+            $note);
     }
 
     public function update($notebookId, $noteId)
@@ -347,7 +366,7 @@ class ApiNotesController extends BaseController
         $validator = Validator::make(Input::all(), ["title" => "required"]);
         if (!$validator->passes()) {
             return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_ERROR,
-              $validator->getMessageBag()->toArray());
+                $validator->getMessageBag()->toArray());
         }
 
         $updateNote = Input::json();
@@ -355,14 +374,14 @@ class ApiNotesController extends BaseController
         $note = Note::find($noteId);
         if (is_null($note)) {
             return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_NOTFOUND,
-              array('item' => 'note', 'id' => $noteId));
+                array('item' => 'note', 'id' => $noteId));
         }
 
         $user =
-          $note->users()->where('users.id', '=', Auth::user()->id)->first();
+            $note->users()->where('users.id', '=', Auth::user()->id)->first();
         if (is_null($user)) {
             return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_NOTFOUND,
-              array('item' => 'user'));
+                array('item' => 'user'));
         }
 
         $previousVersion     = $note->version()->first();
@@ -373,14 +392,14 @@ class ApiNotesController extends BaseController
         ) {
 
             $pureContent =
-              PaperworkHelpers::purgeHtml($updateNote->get("content"));
+                PaperworkHelpers::purgeHtml($updateNote->get("content"));
 
             // TODO: This is a temporary workaround for the content_preview. We need to check, whether there is content or at least one attachment.
             // If there's no content, parse the attachment and set the result as content_preview. This should somehow be done within the DocumentParser, I guess.
             $version = new Version(array(
-              'title'           => $updateNote->get("title"),
-              'content'         => $pureContent,
-              'content_preview' => substr(strip_tags($pureContent), 0, 255)
+                'title'           => $updateNote->get("title"),
+                'content'         => $pureContent,
+                'content_preview' => substr(strip_tags($pureContent), 0, 255)
             ));
 
             $version->save();
@@ -413,7 +432,7 @@ class ApiNotesController extends BaseController
         }
 
         return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_SUCCESS,
-          $note);
+            $note);
     }
 
     private function destroyNote($notebookId, $noteId)
@@ -438,7 +457,8 @@ class ApiNotesController extends BaseController
     public function destroy($notebookId, $noteId)
     {
         $noteIds   =
-          explode(PaperworkHelpers::MULTIPLE_REST_RESOURCE_DELIMITER, $noteId);
+            explode(PaperworkHelpers::MULTIPLE_REST_RESOURCE_DELIMITER,
+                $noteId);
         $responses = array();
         $status    = PaperworkHelpers::STATUS_SUCCESS;
 
@@ -463,7 +483,7 @@ class ApiNotesController extends BaseController
         }
 
         $user =
-          $note->users()->where('users.id', '=', Auth::user()->id)->first();
+            $note->users()->where('users.id', '=', Auth::user()->id)->first();
         if (is_null($user)) {
             // return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_NOTFOUND, array('item'=>'user'));
             return null;
@@ -477,7 +497,7 @@ class ApiNotesController extends BaseController
 
         $toNotebookUser = $toNotebook->users()
                                      ->where('notebook_user.user_id', '=',
-                                       Auth::user()->id)
+                                         Auth::user()->id)
                                      ->first();
         if (is_null($toNotebookUser)) {
             // return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_NOTFOUND, array('item'=>'toNotebookUser'));
@@ -493,7 +513,8 @@ class ApiNotesController extends BaseController
     public function move($notebookId, $noteId, $toNotebookId)
     {
         $noteIds   =
-          explode(PaperworkHelpers::MULTIPLE_REST_RESOURCE_DELIMITER, $noteId);
+            explode(PaperworkHelpers::MULTIPLE_REST_RESOURCE_DELIMITER,
+                $noteId);
         $responses = array();
         $status    = PaperworkHelpers::STATUS_SUCCESS;
 
@@ -513,7 +534,7 @@ class ApiNotesController extends BaseController
     {
         Note::find($noteId)->tags()->attach($toTagId);
         return PaperworkHelpers::apiResponse(PaperworkHelpers::STATUS_SUCCESS,
-          $noteId);
+            $noteId);
     }
 }
 
