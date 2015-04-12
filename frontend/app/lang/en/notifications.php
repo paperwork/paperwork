f46d5549c10904fcafbdddc77f4ddb2e044be17e
Liongold <beimaginativeegroup@gmail.com>
2015-04-12 13:20:37 +0000

    Added File Upload/Delete notifications

--- frontend/app/lang/en/notifications.php
+++ frontend/app/lang/en/notifications.php
@@ -16,7 +16,10 @@ return array(
     'note_saved_successfully' => 'Your note has been saved successfully. ',
     'note_save_failed' => 'Something went wrong while saving your note. Please try again. ',
     'notebook_edit_successfully' => 'Your note has been updated successfully. ',
-    'notebook_edit_failed' => 'Something went wrong while updating your notebook. Please try again. '
+    'notebook_edit_failed' => 'Something went wrong while updating your notebook. Please try again. ',
+    'upload_finished_successfully' => 'Your upload finished successfully. ',
+    'upload_failed' => 'Your upload has failed. Please try again. ',
+    'file_deleted_successfully' => 'Something went wrong while deleting your file. Please try again. '
     
 );
 
