65fe46e478bd735a438dc7e64bf90f4739d46111
weibin <hiweibin@gmail.com>
2015-05-09 12:44:57 +0800

    Add Chinese (Simplified) translations

---
+++ frontend/app/lang/chi-sim/users.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+	'title_login' => '登录',
+	'title_register' => '注册',
+	'title_request' => '忘记密码',
+	'title_reset' => '密码重置',
+	'link_forgot_password' => '忘记密码？',
+	'label_remember_me' => '记住我',
+	'profile_change_password_info' => '输入新密码:',
+);
