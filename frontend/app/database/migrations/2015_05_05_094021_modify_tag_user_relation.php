19c6616aef866ce7569dc92d81c201d0b74e4a7a
Arsenij Łozicki <manwe64@gmail.com>
2015-06-26 20:54:52 +0300

    Opps, that's I who messed up. Reverting

--- frontend/app/database/migrations/2015_05_05_094021_modify_tag_user_relation.php
+++ frontend/app/database/migrations/2015_05_05_094021_modify_tag_user_relation.php
@@ -12,7 +12,10 @@ class ModifyTagUserRelation extends Migration {
 	 */
 	public function up()
 	{
-	    Schema::table('tags', function(Blueprint $table)
+		        Schema::table('tag_user', function(Blueprint $table) {
+            $table->drop();
+        });
+			Schema::table('tags', function(Blueprint $table)
 		{
 			$table->char('user_id', 36);
 			$table->foreign('user_id')->references('id')->on('users');
@@ -30,6 +33,15 @@ class ModifyTagUserRelation extends Migration {
 		{
 			$table->dropColumn('user_id');
 		});
+		Schema::create('tag_user', function(Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->char('user_id', 36);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->char('tag_id', 36);
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+        });
 	}
 
 }
