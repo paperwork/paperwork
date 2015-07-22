ef7b570549e4874e861fe87a3c1b68c80d1c1dd3
JJ <JamborJan@users.noreply.github.com>
2015-07-22 08:54:24 +0200

    ENEX
    The Evernote import is experimental (it works but some files are not processed see https://github.com/twostairs/paperwork/issues/13).
    The Evernote export is not working at all (downloads always a empty 0byte file). 
    Switched description text and changed layout a bit as import button and export headline where very close.

--- frontend/app/views/user/settings/import.blade.php
+++ frontend/app/views/user/settings/import.blade.php
@@ -1,6 +1,6 @@
 [[ Form::open(array('id' => 'form-import', 'class' => 'form', 'role' => 'form', 'files' => true, 'action' => 'UserController@import')) ]]
 	<div class="form-group">
-		<label for="exampleInputFile">[[ Lang::get('messages.user.settings.import.evernotexml') ]]</label>
+		<label for="exampleInputFile">[[ Lang::get('messages.user.settings.import.evernotexml') ]] ([[ Lang::get('keywords.experimental') ]])</label>
 		[[ Form::file('enex'); ]]
 		<p class="help-block">[[ Lang::get('messages.user.settings.import.upload_evernotexml') ]]</p>
 	</div>
@@ -9,14 +9,13 @@
 		<div class="col-sm-offset-5 col-sm-7">
 			[[ Form::submit(Lang::get('keywords.import'), array('id' => 'submit-import', 'class' => 'btn btn-primary')) ]]
 		</div>
-		<p>[[ Lang::get('keywords.not_implemented') ]]</p>
 	</div>
 [[ Form::close() ]]
-
+<br/><br/><br/>
 [[ Form::open(array('id' => 'form-export', 'class' => 'form', 'role' => 'form', 'action' => 'UserController@export'))]]
     <div class="form-group">
         <label class="control-label">
-            [[ Lang::get('messages.user.settings.export.evernotexml') ]] ([[ Lang::get('keywords.experimental') ]])
+            [[ Lang::get('messages.user.settings.export.evernotexml') ]] ([[ Lang::get('keywords.not_implemented') ]])
         </label>
         <p class="help-block">[[ Lang::get('messages.user.settings.export.download_evernotexml') ]]</p>
         <div class="col-sm-offset-5 col-sm-7">
