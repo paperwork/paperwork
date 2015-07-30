9b07e360a5c796e42b6c05ff4ad7da7210df2a49
JJ <JamborJan@users.noreply.github.com>
2015-07-30 05:44:45 +0000

    note_user.umask = 0 causes empty exports
    Valid values for ```note_user.umask``` are:
    
    ```
    const UMASK_READONLY = 4;
    const UMASK_READWRITE = 6;
    const UMASK_OWNER = 7;
    ```
    
    I assume that it should be possible to only export own notes so ```->where('note_user.umask', '=', '7');``` should be right.

--- frontend/app/controllers/UserController.php
+++ frontend/app/controllers/UserController.php
@@ -387,7 +387,7 @@ class UserController extends BaseController
                    ->join('note_user', function ($join) {
                        $join->on('notes.id', '=', 'note_user.note_id')
                             ->where('note_user.user_id', '=', Auth::user()->id)
-                            ->where('note_user.umask', '=', '0');
+                            ->where('note_user.umask', '=', '7');
                    })
                    ->join('notebooks', function ($join) {
                        $join->on('notes.notebook_id', '=', 'notebooks.id');
