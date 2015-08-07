6a21d27335bfad6f2b81d517ec142b64fefc4aa0
Arseniy Lozicki <arseniy.lozicki@voltimum.com>
2015-08-07 15:15:25 +0300

    Fix #109 - Fixed broken html

--- frontend/app/js/paperwork/notes/pw-note-content.directive.js
+++ frontend/app/js/paperwork/notes/pw-note-content.directive.js
@@ -1,5 +1,7 @@
 angular.module('paperworkNotes')
     .directive('pwNoteContent', function ($compile) {
+        var ae = angular.element;
+
         return {
             restrict: 'AE',
             scope: {
@@ -8,20 +10,25 @@ angular.module('paperworkNotes')
             link: function (scope, element) {
                 scope.$watch('content', function () {
                     if (scope.content) {
-                        var content = angular.element(scope.content), code = content.find('code');
+                        var content = ae('<div class="node-content-wrapper">' + scope.content + '</div>'),
+                            code = content.find('code');
 
                         code.each(function () {
-                            var $this = angular.element(this, content);
+                            var $this = ae(this, content);
 
-                            $this.replaceWith(
-                                angular.element('<div>', {
-                                    'class': $this.attr('class'),
-                                    'language': $this.attr('class').replace(/language-/, ''),
-                                    'hljs': true,
-                                    'no-escape': true,
-                                    'html': $this.html()
-                                })
-                            );
+                            if ($this.parent().prop('tagName') === 'PRE') {
+                                $this.unwrap();
+                            }
+
+                            var $codeContainer = ae('<div>', {
+                                'class': $this.attr('class'),
+                                'language': $this.attr('class').replace(/language-/, ''),
+                                'html': $this.html()
+                            });
+
+                            $codeContainer.attr('hljs', '').attr('no-escape', '');
+
+                            $this.replaceWith($codeContainer);
                         });
 
                         element.html(content.html());
