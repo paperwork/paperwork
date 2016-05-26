f391b6a8465926b3c888667a3efd9be5ad9c39ac
Liongold <beimaginativeegroup@gmail.com>
2016-05-26 11:46:42 +0000

    Removed undefined variable errors from registration form

--- frontend/app/views/partials/registration-form.blade.php
+++ frontend/app/views/partials/registration-form.blade.php
@@ -17,7 +17,7 @@
 @endforeach
 
 [[ Form::open(array('class' => 'form-signin', 'role' => 'form')) ]]
-@if (!$ajax)
+@if (!Request::ajax())
 <h1 class="form-signin-heading">[[Lang::get('users.title_register')]]</h1>
 @endif
 <div class="form-group [[ $errors->first('username') ? 'has-error' : '' ]]">
@@ -44,7 +44,7 @@
 <div class="checkbox">
 </div>
 <div class="form-group">
-  @if (!$ajax)
+  @if (!Request::ajax())
   [[ Form::submit(Lang::get('keywords.sign_up'), array('class' => 'btn btn-lg btn-primary btn-block')) ]]
   <a class="btn btn-lg btn-default btn-block" href="[[ URL::route("/")]]">[[Lang::get('keywords.back')]]</a>
   @endif
