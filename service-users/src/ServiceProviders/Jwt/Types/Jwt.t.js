f9bccc6c8c8410be8e4d61e11e02916ac8339ca5
Marius <marius@twostairs.com>
2018-02-11 16:41:32 +0100

    Added JWT types

---
+++ service-users/src/ServiceProviders/Jwt/Types/Jwt.t.js
@@ -0,0 +1,13 @@
+//@flow
+
+export type JwtCredentials = {
+    consumer_id: string,
+    created_at: number,
+    id: string,
+    key: string,
+    secret: string
+};
+
+export type JwtToken = {
+};
+
