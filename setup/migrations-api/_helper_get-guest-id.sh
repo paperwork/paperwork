cecee7ccc8c5c537153c52b9c7b5049aed5a2577
Marius <marius@twostairs.com>
2018-03-10 12:33:53 +0100

    Removing shell script migrations

--- setup/migrations-api/_helper_get-guest-id.sh
+++
@@ -1,6 +0,0 @@
-curl "$1/consumers/00000000-0000-0000-0000-000000000000" \
-     -H 'Content-Type: application/json; charset=utf-8' \
-     -s \
-     | awk 'match($0, /"id":"[0-9a-z\-]+"/) { print substr( $0, RSTART, RLENGTH )}' \
-     | awk -F: '{ print $2 }' \
-     | sed 's/"//g'
